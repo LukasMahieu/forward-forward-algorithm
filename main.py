Fix logging and loss calculation

main.py
@@ -3,6 +3,8 @@ import os
 import torch
 import torch.nn as nn
 
+import numpy as np
+
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
@@ -13,9 +15,8 @@ from networks.unsupervised import ReceptiveFieldNet
 from datasets.unsupervised import create_mnist_datasets
 
 # Hyperparams
-BATCH_SIZE = 1024
-NUM_EPOCHS = 100
-THRESHOLD = 1  # Arbitrary threshold > 0
+BATCH_SIZE = 1000  # So 60 batches since 60k images
+NUM_EPOCHS = 10
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", DEVICE)
 
@@ -36,13 +37,12 @@ writer = SummaryWriter("runs/unsupervised")
 lowest_total_loss = 100
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
+running_batch_idx = 0
+running_total_loss = 0.0
+
 # Training loop
 for epoch in range(1, NUM_EPOCHS + 1):
     model.train()
-
-    running_layer_losses = {}
-    for i, layer in enumerate(model.layers):
-        running_layer_losses[i] = 0.0
 
     progress_bar = tqdm(
         enumerate(zip(positive_loader, negative_loader)),
@@ -52,80 +52,81 @@ for epoch in range(1, NUM_EPOCHS + 1):
     )
 
     for batch_idx, (positive_batch, negative_batch) in progress_bar:
+        batch_layer_losses = {}
+        for i, layer in enumerate(model.layers):
+            batch_layer_losses[i] = 0.0
+
+        running_batch_idx += 1
+
         positive_images, _ = positive_batch
         negative_images, _ = negative_batch
         positive_images = positive_images.to(DEVICE)
         negative_images = negative_images.to(DEVICE)
 
         # Positive pass
-        layer_losses, layer_goodnesses_pos = model.train_batch(positive_images, "pos")
+        layer_losses, layer_goodnesses_pos = model.train_batch(
+            positive_images, "pos"
+        )
 
         for i, layer_loss in enumerate(layer_losses):
-            running_layer_losses[i] += layer_loss
+            batch_layer_losses[i] += layer_loss
 
         # Negative pass
-        layer_losses, layer_goodnesses_neg = model.train_batch(negative_images, "neg")
+        layer_losses, layer_goodnesses_neg = model.train_batch(
+            negative_images, "neg"
+        )
 
         for i, layer_loss in enumerate(layer_losses):
-            running_layer_losses[i] += layer_loss
+            batch_layer_losses[i] += layer_loss
 
         # Logging
         for layer_idx, layer_goodness in enumerate(layer_goodnesses_pos):
             writer.add_scalar(
                 f"Layer {layer_idx} Pos Goodness",
                 layer_goodness,
-                (epoch - 1) * len(positive_loader) + batch_idx,
+                running_batch_idx,
             )
 
         for layer_idx, layer_goodness in enumerate(layer_goodnesses_neg):
             writer.add_scalar(
                 f"Layer {layer_idx} Neg Goodness",
                 layer_goodness,
-                (epoch - 1) * len(positive_loader) + batch_idx,
+                running_batch_idx,
             )
 
         # Print progress every 2 batches
-        if batch_idx % 2 == 0:
-            model_params = model.state_dict()
-            # test saving
-            torch.save(model_params, "models/test.pt")
-
-            total_loss = 0.0
-            for layer_idx, running_loss in running_layer_losses.items():
-                average_loss = running_loss / (batch_idx * 2 + 1)
-                writer.add_scalar(
-                    f"Layer {layer_idx} Average Batch Loss",
-                    average_loss,
-                    (epoch - 1) * len(positive_loader) + batch_idx,
-                )
-                total_loss += average_loss
+        batch_total_loss = 0.0
+        for layer_idx, batch_layer_loss in batch_layer_losses.items():
             writer.add_scalar(
-                f"Total Average Batch Loss",
-                total_loss,
-                (epoch - 1) * len(positive_loader) + batch_idx,
+                f"Layer {layer_idx} Batch Loss",
+                batch_layer_loss,
+                running_batch_idx,
             )
-            postfix = {
-                f"Layer {layer_idx}:": running_loss / (10 * 2)
-                for layer_idx, running_loss in running_layer_losses.items()
-            }
-            progress_bar.set_postfix(postfix)
+            batch_total_loss += batch_layer_loss
 
-    # Calculate average loss for each layer every 5 epochs
-    # and save the model if the average loss accross layers is the lowest so far
+        writer.add_scalar(
+            f"Total Batch Loss",
+            batch_total_loss,
+            running_batch_idx,
+        )
+        postfix = {
+            f"Layer {layer_idx}": batch_layer_loss
+            for layer_idx, batch_layer_loss in batch_layer_losses.items()
+        }
+        progress_bar.set_postfix(postfix)
+
+        running_total_loss += batch_total_loss
+
+    # Save the model every 5 epochs if it's the best so far
     if epoch % 5 == 0:
-        total_loss = 0.0
-        for layer_idx, running_loss in running_layer_losses.items():
-            average_loss = running_loss / (len(positive_loader) + len(negative_loader))
-
-            print(f"Epoch: {epoch}, Average Layer {layer_idx} Loss: {average_loss:.4f}")
-            total_loss += average_loss
-        if total_loss < lowest_total_loss:
-            lowest_total_loss = total_loss
-            if not os.path.exists("models"):
-                os.makedirs("models")
+        average_total_loss = np.round(
+            running_total_loss / running_batch_idx, 3
+        )
+        if average_total_loss < lowest_total_loss:
             torch.save(
                 model.state_dict(),
-                f"models/unsupervised_{timestamp}_{epoch}.pt",
+                f"models/unsupervised_{timestamp}_{epoch}_{average_total_loss}.pt",
             )
 
 writer.close()
+print(f"Finished training at {datetime.now().strftime('%H:%M:%S')}")

main_clf.py
@@ -13,7 +13,9 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = ReceptiveFieldClassifier().to(DEVICE)
 
-pretrained_dict = torch.load("models/test.pt")
+pretrained_dict = torch.load(
+    "models/unsupervised_2023-06-23_00-18-08_5_4.253.pt"
+)
 model_dict = model.state_dict()
 pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
 